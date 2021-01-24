a41c4fd5795d739a982c63a84c5c26fe719d1aac
elenkalee <elena.lipukhina@gmail.com>
2021-01-24 13:47:31 +0300

    first commit

---
+++ CsvFileSource/src/test/java/ru/netology/bonus/BonusServiceTest.java
@@ -0,0 +1,26 @@
+package ru.netology.bonus;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BonusServiceTest {
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/data.csv")
+    @CsvSource(
+            value={
+                    "'registered user, bonus under limit',100060,true,30",
+                    "'registered user, bonus over limit',100000060,true,500",
+                    "'unregistered user, bonus under limit',100060,false,10",
+                    "'unregistered user, bonus over limit',100000060,false,500"
+            }
+    )
+     void shouldCalculate(String test, long amount, boolean registered, long expected) {
+        BonusService service = new BonusService();
+        long actual = service.calculate(amount, registered);
+        assertEquals(expected, actual);
+    }
+}
