4da8459f2e6d205a646e858a8ff8fb41da3f4289
elenkalee <elena.lipukhina@gmail.com>
2021-02-04 22:37:35 +0300

    added data.csv and update BonusServiceTest

--- CsvFileSource/src/test/java/ru/netology/bonus/BonusServiceTest.java
+++ CsvFileSource/src/test/java/ru/netology/bonus/BonusServiceTest.java
@@ -2,23 +2,14 @@ package ru.netology.bonus;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
-import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BonusServiceTest {
 
     @ParameterizedTest
-    @CsvFileSource(resources = "/data.csv")
-    @CsvSource(
-            value={
-                    "'registered user, bonus under limit',100060,true,30",
-                    "'registered user, bonus over limit',100000060,true,500",
-                    "'unregistered user, bonus under limit',100060,false,10",
-                    "'unregistered user, bonus over limit',100000060,false,500"
-            }
-    )
-     void shouldCalculate(String test, long amount, boolean registered, long expected) {
+    @CsvFileSource (resources = "/data.csv", delimiter = '|')
+      public void shouldCalculate (String ParametrizedTest, long amount, boolean registered, long expected) {
         BonusService service = new BonusService();
         long actual = service.calculate(amount, registered);
         assertEquals(expected, actual);
